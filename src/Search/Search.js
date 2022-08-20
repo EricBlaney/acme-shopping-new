caac49bf5231cb6ce6028de188a0eae768eb9b9b
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2022-08-20 09:18:46 -0400

    Fix minor issues

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Search = ({ history }) => {
     return (
-      <input className='searchbar' onChange={ev => ev.target.value ? history.push(`/search/${ev.target.value}`): history.push('/')} placeholder='Enter Search Here'/>
+      <input className='searchbar' onChange={ev => ev.target.value ? history.push(`/search/${ev.target.value}`) : history.push('/')} placeholder='Enter Game Here'/>
     )
   }
 
