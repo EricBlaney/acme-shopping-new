057e0e8c2573bc3c1fd046f9859e1554c1a6398a
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2022-08-27 10:34:47 -0400

    source modal

--- routes/orders.js
+++ routes/orders.js
@@ -82,15 +82,10 @@ app.post('/create-checkout-session', async(req, res)=> {
   const session = await stripe.checkout.sessions.create({
     line_items: req.body,
     mode: 'payment',
-
     success_url: `https://retros-gaming.herokuapp.com/#/cart/success`,
-    // success_url: process.env.CHECKOUT_SUCCESS_URL,
     cancel_url: `https://retros-gaming.herokuapp.com/#/cart/cancel`,
+    // success_url: process.env.CHECKOUT_SUCCESS_URL,
     // cancel_url: process.env.CHECKOUT_CANCEL_URL,
-//     success_url: `https://retros-gaming.herokuapp.com/#/cart/success`,
-//     cancel_url: `http://localhost:3000/#/cart/cancel`,
-    success_url: process.env.CHECKOUT_SUCCESS_URL,
-    cancel_url: process.env.CHECKOUT_CANCEL_URL,
 
   });
   res.json({ url: session.url })
